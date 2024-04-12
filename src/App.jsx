dd7a32aa9726f3cc9d53a061e2c547ae2d6e5bf0
mafkar2 <mafkar2@gmail.com>
2024-04-11 21:58:48 -0700

    initialized project3

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import  NavBar  from './components/NavBar';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+      <div>
+
+        test number one
+       </div>
+                        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+
+function NavBar() {
+
+  return (
+      <nav className="navbar">
+      <h1>The Tutorail Website</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/faq">FAQ</a>
+        <a href="/login">Login</a>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar;
